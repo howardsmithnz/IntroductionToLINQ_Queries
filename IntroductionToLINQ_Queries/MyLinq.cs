4ddafa0b8d60599eed69453e119781076561a898
Howard Smith <howard.smith@finnz.com>
2019-12-17 11:30:21 +1300

    Add Random method to MyLinq

--- IntroductionToLINQ_Queries/MyLinq.cs
+++ IntroductionToLINQ_Queries/MyLinq.cs
@@ -8,6 +8,15 @@ namespace IntroductionToLINQ_Queries
 {
     public static class MyLinq
     {
+        public static IEnumerable<double> Random()
+        {
+            var random = new Random();
+            while (true)
+            {
+                yield return random.NextDouble();
+            }
+        }
+
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate) // extension method on IEnumerable
         {
             //var result = new List<T>();
--- IntroductionToLINQ_Queries/Program.cs
+++ IntroductionToLINQ_Queries/Program.cs
@@ -10,6 +10,12 @@ namespace IntroductionToLINQ_Queries
     {
         static void Main(string[] args)
         {
+            var numbers = MyLinq.Random().Where(n => n > 0.5).Take(10);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
             var movies = new List<Movie>
             {
                 new Movie { Title = "The Dark Knight", Rating = 8.9f, Year = 2008},
@@ -18,12 +24,20 @@ namespace IntroductionToLINQ_Queries
                 new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980}
             };
 
-            var query = movies.Where(m => m.Year > 2000);
+            var query = movies.Where(m => m.Year > 2000).ToList(); // ToList() will force execution - no deferred execution
 
-            foreach (var movie in query)
+            Console.WriteLine(query.Count());
+
+            var enumerator = query.GetEnumerator();
+            while (enumerator.MoveNext())
             {
-                Console.WriteLine(movie.Title);
+                Console.WriteLine(enumerator.Current.Title);
             }
+
+            //foreach (var movie in query)
+            //{
+            //    Console.WriteLine(movie.Title);
+            //}
         }
     }
 }
