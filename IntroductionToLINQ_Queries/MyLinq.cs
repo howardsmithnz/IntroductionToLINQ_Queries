b94ec5d106b6cf2e903a69d39ad6c2e9354ef5cb
Howard Smith <howard.smith@finnz.com>
2019-12-17 09:48:37 +1300

    Implement Filter extension method using yield

--- IntroductionToLINQ_Queries/MyLinq.cs
+++ IntroductionToLINQ_Queries/MyLinq.cs
@@ -8,18 +8,26 @@ namespace IntroductionToLINQ_Queries
 {
     public static class MyLinq
     {
-        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate) // extension method on IEnumerable
         {
-            var result = new List<T>();
+            //var result = new List<T>();
+
+            //foreach (var item in source)
+            //{
+            //    if (predicate(item))
+            //    {
+            //        result.Add(item);
+            //    }
+            //}
+            //return result;
 
             foreach (var item in source)
             {
                 if (predicate(item))
                 {
-                    result.Add(item);
+                    yield return item;
                 }
             }
-            return result;
         }
     }
 }
