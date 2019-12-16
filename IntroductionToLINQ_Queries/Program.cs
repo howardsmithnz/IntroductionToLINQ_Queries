93b562b1fc9c0df912a61a3aad7c487eea8f63f0
Howard Smith <howard.smith@finnz.com>
2019-12-16 16:43:49 +1300

    First commit - create classes

--- IntroductionToLINQ_Queries/Program.cs
+++ IntroductionToLINQ_Queries/Program.cs
@@ -10,6 +10,13 @@ namespace IntroductionToLINQ_Queries
     {
         static void Main(string[] args)
         {
+            var movies = new List<Movie>
+            {
+                new Movie { Title = "The Dark Knight", Rating = 8.9f, Year = 2008},
+                new Movie { Title = "The King's Speech", Rating = 8.0f, Year = 2010},
+                new Movie { Title = "Casablanca", Rating = 8.5f, Year = 1942},
+                new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980}
+            };
         }
     }
 }
